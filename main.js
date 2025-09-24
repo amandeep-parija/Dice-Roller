5ec80e40544a7d097f8a8f16976016840010430d
amandeep-parija <93464533+amandeep-parija@users.noreply.github.com>
2025-09-24 18:30:26 +0530

    Logic push

--- main.js
+++ main.js
@@ -1,10 +1,52 @@
 const buttonEl = document.getElementById("roll-button");
-const diceEl = document.getElementById("dice")
+const diceEl = document.getElementById("dice");
+const rollListEl = document.getElementById("roll-list");
+
+
+let historyList = [];
+
+
+
+function rollDice(){
+    const rollResult = Math.floor(Math.random() * 6) + 1;
+    const diceFace = getDiceFace(rollResult);
+    diceEl.innerHTML = diceFace;
+    historyList.push(rollResult);
+    updateRollHistory();
+}
+
+function updateRollHistory(){
+    rollListEl.innerHTML = '';
+    for(let i = 0; i< historyList.length; i++){
+        const listItem = document.createElement("li")
+        listItem.innerHTML = `Roll ${i+ 1}: <span> ${getDiceFace(historyList[i])} </span>`;
+        rollListEl.appendChild(listItem)
+    }
+}
+
+function getDiceFace(rollResult){
+    switch(rollResult){
+        case 1:
+            return ' <i class="fa-solid fa-dice-one"></i> ';
+        case 2:
+            return '<i class="fa-solid fa-dice-two"></i>';
+        case 3:
+            return '<i class="fa-solid fa-dice-three"></i>';
+        case 4:
+            return '<i class="fa-solid fa-dice-four"></i>';
+        case 5:
+            return '<i class="fa-solid fa-dice-five"></i>';
+        case 6:
+            return '<i class="fa-solid fa-dice-six"></i>';
+        default:
+            return '';
+    }
+}
 
 buttonEl.addEventListener('click', () =>{
-    diceEl.classList.remove("roll-animation");
-
-    void diceEl.offsetWidth;
-    
     diceEl.classList.add("roll-animation");
+    setTimeout(() => {
+        diceEl.classList.remove("roll-animation");
+        rollDice();
+    }, 1000);
 })
